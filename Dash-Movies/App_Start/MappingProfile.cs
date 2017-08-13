a1f502cf60a66ed6c7f0170bd094d523898c2d45
alfredoz28 <alfredoz28mm@gmail.com>
2017-08-13 10:51:04 -0400

    Hola
    Mundo

---
+++ Dash-Movies/App_Start/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Dash_Movies.Dtos;
+using Dash_Movies.Models;
+
+namespace Dash_Movies.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+
+    }
+}
---
+++ Dash-Movies/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Dash_Movies.Models;
+
+namespace Dash_Movies.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
